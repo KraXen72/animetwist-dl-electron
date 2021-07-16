856abb2e5252ec49bb821702a5f0cb61b87e14ce
KraXen72 <dpelech1@gmail.com>
2021-07-16 15:28:42 +0200

    some progress on new gui

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1200,
+    height: 750,
+    minWidth: 900,
+    minHeight: 490,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
